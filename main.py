b52ac7d813840f5e961b068b57e211e06b6ca1cf
lalobgc <lalobg.c@gmail.com>
2025-03-24 11:09:16 -0600

    main.py improved for the first step

--- main.py
+++ main.py
@@ -5,13 +5,11 @@ from tkinter import BOTH, Canvas, Tk
 
 class Window:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.canvas = Canvas()
-        self.canvas.pack()
-        self.running = False
+        self.__canvas = Canvas(self.__root, width=width, height=height)
+        self.__canvas.pack(fill=BOTH, expand=True)
+        self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
@@ -19,12 +17,12 @@ class Window:
         self.__root.update()
 
     def wait_for_close(self):
-        self.running = True
-        while self.running:
+        self.__running = True
+        while self.__running:
             self.redraw()
 
     def close(self):
-        self.running = False
+        self.__running = False
 
 
 def main():
